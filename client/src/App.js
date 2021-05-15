1478bcaa47a1879c1c567c9c883ffd719d542373
Chirag Khandhar <ckhandhar@hawk.iit.edu>
2021-05-14 22:22:12 -0500

    Create system theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
+import { Materials } from "./Components/Materials";
+
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Materials/>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/Components/Materials.js
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from 'react'
+
+// Import Styles
+import "../Styles/Materials.css"
+
+export class Materials extends Component {
+    render() {
+        return (
+            <Fragment >
+                <div theme = "cesium" className = "material-container">
+
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default Materials
